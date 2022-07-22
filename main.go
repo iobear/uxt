8fa956ea4925b6f42d3586782d3321d420172d4d
Bjorn Jorgensen <bjoj@stofa.dk>
2022-07-22 22:55:59 +0200

    Change help txt

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 	if argumentsAmount > 0 {
 		input := os.Args[1]
-		if os.Args[1] == "help" {
+		if os.Args[1] == "help" || os.Args[1] == "-h" {
 			printHelp()
 			os.Exit(0)
 		}
@@ -37,6 +37,7 @@ func main() {
 			}
 			finalTime := now + int64(inputInt)
 			fmt.Println(finalTime)
+
 		} else {
 			if inputInt, err := strconv.Atoi(input); err == nil {
 				var unixTime int64 = int64(inputInt)
@@ -63,12 +64,17 @@ func main() {
 }
 
 func printHelp() {
-	helpTxt := "This is the help you get  \n\n"
-	helpTxt += os.Args[0] + "\n\n"
-	helpTxt += os.Args[0] + " +<int> or -<int>\n\n"
+	helpTxt := "This is all the help you get  \n\n"
+	helpTxt += os.Args[0] + "\n"
+	helpTxt += " 946684800\n\n"
+	helpTxt += os.Args[0] + " +<int> or -<int>\n"
+	helpTxt += " 946684810\n\n"
 	helpTxt += os.Args[0] + " <Unix Epoch> \n"
-	helpTxt += os.Args[0] + " <Unix Epoch> RFC3339\n\n"
-	helpTxt += os.Args[0] + " version\n\n"
+	helpTxt += " Thu May  8 08:13:30 CEST 2025\n\n"
+	helpTxt += os.Args[0] + " <Unix Epoch> RFC3339\n"
+	helpTxt += " 2025-05-08T08:13:30+02:00\n\n"
+	helpTxt += os.Args[0] + " version\n"
+	helpTxt += " 0.0.x\n\n"
 	helpTxt += "https://github.com/iobear/uxt \n"
 
 	fmt.Println(helpTxt)
